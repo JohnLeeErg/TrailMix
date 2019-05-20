8ce3d995c2309f17bb01ead7d4edc9862cb378ae
Michael Guattery <guattery@sheridancollege.ca>
2019-05-20 17:06:01 -0400

    Camera FPS fixes - old speed before using delta time was .1

---
+++ Puzz for Two/Assets/FpsDebug.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsDebug : MonoBehaviour
+{
+    [SerializeField] Text renderFPSText;
+    [SerializeField] Text physicsFPSText;
+
+
+    float frameCount = 0;
+    float dt = 0.0f;
+    float fps = 0.0f;
+
+    float frameCountP = 0;
+    float dtP = 0.0f;
+    float fpsP = 0.0f;
+
+    float updateRate = 4.0f;  // 4 updates per sec.
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCount++;
+        dt += Time.deltaTime;
+        if (dt > 1.0f / updateRate)
+        {
+            fps = frameCount / dt;
+            frameCount = 0;
+            dt -= 1.0f / updateRate;
+        }
+
+        renderFPSText.text = "RenderFPS: " + Mathf.RoundToInt(fps);
+    }
+
+    private void FixedUpdate()
+    {
+        frameCountP++;
+        dtP += Time.fixedDeltaTime;
+        if (dtP > 1.0f / updateRate)
+        {
+            fpsP = frameCountP / dtP;
+            frameCountP = 0;
+            dtP -= 1.0f / updateRate;
+        }
+
+        physicsFPSText.text = "PhysicsFPS: " + Mathf.RoundToInt(fpsP);
+    }
+}

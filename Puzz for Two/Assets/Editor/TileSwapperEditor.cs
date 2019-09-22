8b91c8bb490c7031a65c21480809992c0ad42835
Michael Guattery <guattery@sheridancollege.ca>
2019-09-22 15:10:19 -0400

    Tile editor fixes

--- Puzz for Two/Assets/Editor/TileSwapperEditor.cs
+++ Puzz for Two/Assets/Editor/TileSwapperEditor.cs
@@ -2,18 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Tilemaps;
 
 [CustomEditor(typeof(TileSwapper))]
-public class TileSwapperEditor : Editor {
+[CanEditMultipleObjects]
+public class TileSwapperEditor : Editor
+{
+    Texture2D[] m_Textures; //test if it fixes the render issuelk
 
     public override void OnInspectorGUI()
     {
+        SerializedProperty tilesReplacedProperty = this.serializedObject.FindProperty("tilesToReplace");
+        SerializedProperty tilesFilledProperty = this.serializedObject.FindProperty("tilesToFill");
+
         DrawDefaultInspector();
 
         TileSwapper myScript = (TileSwapper)target;
-        if (GUILayout.Button("Swap Tile Sprites"))
+
+        tilesReplacedProperty.arraySize = EditorGUILayout.IntField("Number of Tiles", tilesReplacedProperty.arraySize);
+
+        serializedObject.ApplyModifiedProperties();
+
+        if (tilesReplacedProperty.arraySize > 0)
         {
-            myScript.SwapTiles();
+            if (GUILayout.Button("Swap All Tile Sprites", GUILayout.Height(30)))
+            {
+                myScript.SwapTiles(-1);
+            }
+
+            for (int i = 0; i < tilesReplacedProperty.arraySize; i++)
+            {
+
+                EditorGUILayout.BeginVertical("Button");
+                EditorGUILayout.BeginHorizontal();
+
+                if (myScript.tilesToReplace[i] != null)
+                {
+                    Texture2D myTexture;
+                    myTexture = AssetPreview.GetAssetPreview(myScript.tilesToReplace[i]);
+                    GUILayout.Label(myTexture, GUILayout.Width(50), GUILayout.Height(50));
+                }
+                else
+                {
+                    GUILayout.Label("Missing Tile " + i);
+                }
+
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Swap To ->"); //center this text
+                GUILayout.FlexibleSpace();
+
+                if (i < myScript.tilesToFill.Length)
+                {
+                    if (myScript.tilesToFill[i] != null)
+                    {
+                        Texture2D myTextureFill;
+                        myTextureFill = AssetPreview.GetAssetPreview(myScript.tilesToFill[i]);
+                        GUILayout.Label(myTextureFill, GUILayout.Width(50), GUILayout.Height(50));
+                    }
+                    else
+                    {
+                        GUILayout.Label("Missing Tile " + i);
+                    }
+                }
+                else
+                {
+                    GUILayout.Label("OUT OF RANGE");
+                }
+
+                EditorGUILayout.EndHorizontal();
+
+                SerializedProperty m_TileTestReplace = tilesReplacedProperty.GetArrayElementAtIndex(i);
+                EditorGUILayout.PropertyField(m_TileTestReplace, new GUIContent("Tile To Swap"));
+
+                SerializedProperty m_TileTestFill = tilesFilledProperty.GetArrayElementAtIndex(i);
+                EditorGUILayout.PropertyField(m_TileTestFill, new GUIContent("Tile To Change To"));
+
+                serializedObject.ApplyModifiedProperties();
+
+                Color c = GUI.backgroundColor;
+
+                if (m_TileTestReplace.objectReferenceValue != null && m_TileTestFill.objectReferenceValue != null)
+                {
+                    GUI.backgroundColor = Color.green;
+                    if (GUILayout.Button("Swap " + m_TileTestReplace.objectReferenceValue.name + " with " + m_TileTestFill.objectReferenceValue.name))
+                    {
+                        myScript.SwapTiles(i);
+                    }
+                }
+                else
+                {
+                    GUI.backgroundColor = Color.red;
+                    if (GUILayout.Button("Cannot swap with Null tile value"))
+                    {
+                        Debug.LogError("You cannot swap a tile with a null value, or vice versa. Please check element " + i + " of your Tile Swapper.");
+                    }
+                }
+
+                GUI.backgroundColor = c; //reset color to norm
+                EditorGUILayout.EndVertical();
+            }
         }
     }
 }
--- Puzz for Two/Assets/Scripts/Misc/TileSwapper.cs
+++ Puzz for Two/Assets/Scripts/Misc/TileSwapper.cs
@@ -5,36 +5,96 @@ using UnityEngine.Tilemaps;
 
 public class TileSwapper : MonoBehaviour
 {
-    Tilemap myTiles;
-    TileBase[] allTilesInMap; //the best part is this is non runtime so I can be sloppppy
+    public enum TargetType
+    {
+        Self,
+        Children,
+    }
+
+    public TargetType target;
+
+    [HideInInspector]
+    public bool canRevert;
+
+    public Tile[] tilesToReplace;
+    public Tile[] tilesToFill;
+
+    [HideInInspector]
+    public List<Tilemap> tileMaps = new List<Tilemap>();
+    TileBase[] allTilesInMap;
     BoundsInt bounds;
-    [SerializeField] OutLineType tileToReplace;
-    [SerializeField] OutLineType tileToFill;
+
+    //Dictionaries for Tiles to replace
     Dictionary<Tile, int> tileToReplaceDict = new Dictionary<Tile, int>();
     Dictionary<int, Tile> tileToFillDict = new Dictionary<int, Tile>();
 
-    public void SwapTiles()
+    public void SwapTiles(int index)
     {
-        myTiles = GetComponent<Tilemap>();
-        myTiles.CompressBounds();
-        tileToReplaceDict.Clear();
-        tileToFillDict.Clear();
-        FillTileDictionary();
-        bounds = myTiles.cellBounds;
-        foreach (Vector3Int position in myTiles.cellBounds.allPositionsWithin)
-        {
-            if (doesTileExist(position))
-            {
-                UpdateSprite(position);
-            }
-        }
+        Tilemap myTiles;
+        tileMaps.Clear();
 
-        myTiles.RefreshAllTiles();
+        switch (target)
+        {
+            case TargetType.Self:
+                myTiles = GetComponent<Tilemap>();
+                if (myTiles)
+                {
+                    myTiles.CompressBounds();
+                    tileToReplaceDict.Clear();
+                    tileToFillDict.Clear();
+                    FillTileDictionary(index);
+                    bounds = myTiles.cellBounds;
+                    foreach (Vector3Int position in myTiles.cellBounds.allPositionsWithin)
+                    {
+                        if (doesTileExist(position, myTiles))
+                        {
+                            UpdateSprite(position, myTiles);
+                        }
+                    }
+                    myTiles.RefreshAllTiles();
+                }
+                else
+                {
+                    Debug.LogError("TileSwapper is Missing a Tilemap Reference on it's own Object");
+                }
+                break;
+
+            case TargetType.Children:
+                foreach (Transform t in transform)
+                {
+                    Tilemap childTileMap = t.GetComponent<Tilemap>();
+                    if (childTileMap)
+                    {
+                        tileMaps.Add(childTileMap);
+                    }
+                }
+                for (int i = 0; i < tileMaps.Count; i++)
+                {
+                    myTiles = tileMaps[i].GetComponent<Tilemap>();
+                    if (myTiles)
+                    {
+                        myTiles.CompressBounds();
+                        tileToReplaceDict.Clear();
+                        tileToFillDict.Clear();
+                        FillTileDictionary(index);
+                        bounds = myTiles.cellBounds;
+                        foreach (Vector3Int position in myTiles.cellBounds.allPositionsWithin)
+                        {
+                            if (doesTileExist(position, myTiles))
+                            {
+                                UpdateSprite(position, myTiles);
+                            }
+                        }
+                        myTiles.RefreshAllTiles();
+                    }
+                }
+                break;
+        }
     }
 
-    bool doesTileExist(Vector3Int pos)
+    bool doesTileExist(Vector3Int pos, Tilemap tileMapReference)
     {
-        if (myTiles.GetTile(pos))
+        if (tileMapReference.GetTile(pos))
         {
             return true;
         }
@@ -44,26 +104,37 @@ public class TileSwapper : MonoBehaviour
         }
     }
 
-    void UpdateSprite(Vector3Int pos)
+    void UpdateSprite(Vector3Int pos, Tilemap tileMapReference)
     {
-        TileBase tile = myTiles.GetTile(pos);
+        TileBase tile = tileMapReference.GetTile(pos);
         if (tileToReplaceDict.ContainsKey((Tile)tile))
         {
-            myTiles.SetTile(pos, tileToFillDict[tileToReplaceDict[(Tile)tile]]);
+            tileMapReference.SetTile(pos, tileToFillDict[tileToReplaceDict[(Tile)tile]]);
         }
     }
 
-    void FillTileDictionary()
+    void FillTileDictionary(int index)
     {
-        for (int i = 0; i < tileToReplace.variants.Length; i++)
+        if (index < 0) //if -1, assume all
         {
-            if(i >= tileToFill.variants.Length)
+            for (int i = 0; i < tilesToReplace.Length; i++)
             {
-                Debug.LogWarning("Your tileToFill scriptable object you're trying to swap with is smaller than your tileToReplace. Did not replace tiles beyond tileToFill Element " + (i - 1));
-                break;
+                if (i >= tilesToFill.Length)
+                {
+                    Debug.LogWarning("Your tileToFill scriptable object you're trying to swap with is smaller than your tileToReplace. TileSwapper did not replace tiles beyond tileToFill Element " + (i - 1));
+                    break;
+                }
+                if (tilesToReplace[i] != null && tilesToFill[i] != null)
+                {
+                    tileToReplaceDict.Add(tilesToReplace[i], i);
+                    tileToFillDict.Add(i, tilesToFill[i]);
+                }
             }
-            tileToReplaceDict.Add(tileToReplace.variants[i], i);
-            tileToFillDict.Add(i, tileToFill.variants[i]);
+        }
+        else if (index >= 0)
+        {
+            tileToReplaceDict.Add(tilesToReplace[index], index);
+            tileToFillDict.Add(index, tilesToFill[index]);
         }
     }
 }
